491aa2802d7bd7bd03913e584338a08dc729a929
YoruichiBankai <shahzadbankai@gmail.com>
2026-03-25 19:29:17 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+{
+  "name": "2048 Custom App",
+  "short_name": "2048 Custom",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#faf8ef",
+  "theme_color": "#faf8ef",
+  "lang": "de",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
